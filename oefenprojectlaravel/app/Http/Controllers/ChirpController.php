a5f35b403539924a48d4ee10a1bc3663b2e2775e
buttercup2007 <2215907@talnet.nl>
2026-04-02 10:03:13 +0200

    chirp is now in the database

--- oefenprojectlaravel/app/Http/Controllers/ChirpController.php
+++ oefenprojectlaravel/app/Http/Controllers/ChirpController.php
@@ -1,12 +1,22 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\Chirp;
 class ChirpController extends Controller
 {
 public function index()
 {
+    $chirps = Chirp::latest()->get();
+
+    return view('home', compact('chirps'));
+}
+}
+
+{
 return view('home');
 }
-}
+
+
+
 
 
