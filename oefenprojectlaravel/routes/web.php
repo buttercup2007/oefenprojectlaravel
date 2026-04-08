1f32db442d5a4460b1402fc5d6d99843f0ba8ddb
buttercup2007 <2215907@talnet.nl>
2026-04-08 12:25:47 +0200

    edit blade .php and 255 length works

--- oefenprojectlaravel/routes/web.php
+++ oefenprojectlaravel/routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
+
 Route::get('/', [ChirpController::class, 'index']);
 Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
